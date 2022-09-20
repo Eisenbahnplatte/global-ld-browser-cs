b9d822ff78256309a5ba86c4b3e9b784ef37e464
Eisenbahnplatte <18533014+Eisenbahnplatte@users.noreply.github.com>
2022-09-20 16:02:30 +0200

    updated config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 module.exports = defineConfig({
-  publicPath: './',
   transpileDependencies: true,
   configureWebpack: {
     plugins: [new NodePolyfillPlugin()],
