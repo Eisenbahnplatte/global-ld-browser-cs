1559475e3cd7b58f18490543abcd3716504183b1
Eisenbahnplatte <18533014+Eisenbahnplatte@users.noreply.github.com>
2022-09-22 11:17:49 +0200

    config new try

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 module.exports = defineConfig({
-  publicPath: process.env.BASE_URL,
+  publicPath: "https://dbpedia.link/browse/",
   assetsDir: process.env.BASE_URL,
   transpileDependencies: true,
   configureWebpack: {
